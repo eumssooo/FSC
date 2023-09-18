2779518d8920ff54d2c7ba90d465a8916e6b4d85
Teangoo <lty0314@gmail.com>
2023-09-18 23:27:16 +0900

    :sparkles: 댓글 조회 서비스 추가

--- src/main/java/com/example/fsc/service/CommentService.java
+++ src/main/java/com/example/fsc/service/CommentService.java
@@ -1,10 +1,38 @@
 package com.example.fsc.service;
 
 
+import com.example.fsc.dto.CommentDto;
+import com.example.fsc.dto.CommentViewDto;
+import com.example.fsc.entity.CommentEntity;
+import com.example.fsc.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public ResponseEntity<List<CommentViewDto>> postComment(Long post_id) {
+        List<CommentEntity> commentEntityList = commentRepository.findByPostIdOrderByCreatedAtAsc(post_id);
+        List<CommentViewDto> commentDtoList = new ArrayList<>();
+        for (CommentEntity comment: commentEntityList) {
+            CommentViewDto dto = CommentViewDto.builder()
+                    .commentId(comment.getCommentId())
+                    .content(comment.getContent())
+                    .author(comment.getAuthor())
+                    .postId(comment.getPostId())
+                    .createAt(comment.getCreatedAt())
+                    .build();
+            commentDtoList.add(dto);
+        }
+        return ResponseEntity.status(200).body(commentDtoList);
+    }
+
+
 }
