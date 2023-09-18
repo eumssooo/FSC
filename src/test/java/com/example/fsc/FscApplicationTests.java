390853b3163f2ca96b4da9ad95a30b69cba74d5f
eumssooo <mamma1115@gmail.com>
2023-09-18 15:58:27 +0900

    :tada:

---
+++ src/test/java/com/example/fsc/FscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
