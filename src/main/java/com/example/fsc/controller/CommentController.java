38897a8f1d475ea649f33194206c4194682109c7
Teangoo <lty0314@gmail.com>
2023-09-18 23:21:36 +0900

    :sparkles: 댓글 조회 컨트롤러 생성

--- src/main/java/com/example/fsc/controller/CommentController.java
+++ src/main/java/com/example/fsc/controller/CommentController.java
@@ -2,12 +2,24 @@ package com.example.fsc.controller;
 
 
 
+import com.example.fsc.dto.CommentDto;
+import com.example.fsc.dto.CommentViewDto;
+import com.example.fsc.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class CommentController {
+    private final CommentService commentService;
+    @GetMapping("/comments")
+    public ResponseEntity<List<CommentViewDto>> commentView(@RequestParam("post_id") Long post_id){
+       return commentService.postComment(post_id);
+    }
+
 }
