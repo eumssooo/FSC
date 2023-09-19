1879a774b81094756755e79b1b0bec84ce24fc71
eumssooo <mamma1115@gmail.com>
2023-09-19 11:44:08 +0900

    :sparkles: Board 도메인 생성

---
+++ src/main/java/com/example/fsc/domain/Board.java
@@ -0,0 +1,29 @@
+package com.example.fsc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "board")
+public class Board {
+
+    @Id @GeneratedValue
+    @Column(name = "board_id")
+    private Long boardId;
+
+//    private Long emailId;
+
+    private String author;
+
+    private String title;
+
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
