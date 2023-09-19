7c795f71c3445ff713c4bdbc9ae69aee860f8f8a
eumssooo <mamma1115@gmail.com>
2023-09-19 21:25:56 +0900

    :sparkles: BoardService 생성

---
+++ src/main/java/com/example/fsc/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.example.fsc.service;
+
+import com.example.fsc.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+}
+
+
+
