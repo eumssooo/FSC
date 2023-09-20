8657b35be621fa57533c4a5036954682ce0f9ae4
sojinb <thwls3791@naver.com>
2023-09-20 15:27:05 +0900

    :sparkles: 게시물 전체 조회 기능 추가

--- src/main/java/com/example/fsc/controller/BoardController.java
+++ src/main/java/com/example/fsc/controller/BoardController.java
@@ -1,7 +1,26 @@
 package com.example.fsc.controller;
 
+import com.example.fsc.dto.BoardDTO;
+import com.example.fsc.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    //게시물 전체 조회
+    @GetMapping( "/posts")
+    public ResponseEntity<List<BoardDTO>> findAll(){
+        return boardService.findAll();
+    }
+
 }
--- src/main/java/com/example/fsc/service/BoardService.java
+++ src/main/java/com/example/fsc/service/BoardService.java
@@ -1,14 +1,41 @@
 package com.example.fsc.service;
 
+import com.example.fsc.domain.Board;
+import com.example.fsc.dto.BoardDTO;
 import com.example.fsc.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.RequestEntity.put;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
 
+    public ResponseEntity<List<BoardDTO>> findAll() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (Board board : boardList) {
+            BoardDTO boardDTO = BoardDTO.builder()
+                    .boardId(board.getBoardId())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .author(board.getAuthor())
+                    .emailId(board.getEmailId())
+                    .createdAt(board.getCreatedAt())
+                    .build();
+            boardDTOList.add(boardDTO);
+        }
+        return ResponseEntity.status(200).body(boardDTOList);
+    }
 }
 
 
