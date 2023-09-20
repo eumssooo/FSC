8f1085b1a5535a3a44e14f0acb95982ef71e6bee
eumssooo <mamma1115@gmail.com>
2023-09-20 15:34:47 +0900

    :sparkles: 게시물 작성자 검색 Repository 추가

--- src/main/java/com/example/fsc/repository/BoardRepository.java
+++ src/main/java/com/example/fsc/repository/BoardRepository.java
@@ -3,6 +3,9 @@ package com.example.fsc.repository;
 import com.example.fsc.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findBoardsByAuthorContainingOrderByCreatedAtDesc(String email);
 
 }
