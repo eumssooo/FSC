d662769c1f89b721cc5e6ef9e540281e4c963a4b
KimDaHee <hee0zzang@gmail.com>
2023-09-21 16:36:05 +0900

    :art: security 제약조건 수정

--- src/main/java/com/example/fsc/config/SecurityConfiguration.java
+++ src/main/java/com/example/fsc/config/SecurityConfiguration.java
@@ -1,48 +1,55 @@
 package com.example.fsc.config;
 
-//import com.example.fsc.filter.JwtAuthenticationFilter;
+import com.example.fsc.security.JwtAuthenticationFilter;
+import com.example.fsc.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private JwtTokenProvider jwtTokenProvider;
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.headers().frameOptions().sameOrigin()
-                .and()
-                //.authorizeRequests()
-                //.antMatchers("/api/**").permitAll() // 로그인 페이지에 대한 접근 허용
-                //.anyRequest().authenticated() // 그 외 모든 요청은 인증이 필요
-                //.and()
-                .formLogin().disable()
-                //.loginPage("/login").permitAll().and()
-                //.logout().logoutUrl("/logout").permitAll().and()
-                .csrf().disable()
-                .httpBasic().disable()
-                .rememberMe().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                //.and().exceptionHandling().authenticationEntryPoint(new FailedPoint());
+//        http.headers().frameOptions().sameOrigin()
+//                .and()
+//                //.authorizeRequests()
+//                //.antMatchers("/api/**").permitAll() // 로그인 페이지에 대한 접근 허용
+//                //.anyRequest().authenticated() // 그 외 모든 요청은 인증이 필요
+//                //.and()
+//                .formLogin().disable()
+//                //.loginPage("/login").permitAll().and()
+//                //.logout().logoutUrl("/logout").permitAll().and()
+//                .csrf().disable()
+//                .httpBasic().disable()
+//                .rememberMe().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                //.and().exceptionHandling().authenticationEntryPoint(new FailedPoint());
 
-         //http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/login", "/api/signup").permitAll()
+                .anyRequest().hasRole("USER")
+                .and()
+                .exceptionHandling()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
         return  http.build();
     }
 
