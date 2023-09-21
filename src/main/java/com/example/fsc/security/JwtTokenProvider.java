019e8195d8285781363783512bc091b781167daf
KimDaHee <hee0zzang@gmail.com>
2023-09-21 16:35:30 +0900

    :sparkles: 토큰 제약조건 추가

--- src/main/java/com/example/fsc/security/JwtTokenProvider.java
+++ src/main/java/com/example/fsc/security/JwtTokenProvider.java
@@ -1,24 +1,21 @@
 package com.example.fsc.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.yaml.snakeyaml.tokens.Token;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-
 
     public String create(Long emailId, String email){
         //한시간 만료 시간 설정
@@ -33,18 +30,32 @@ public class JwtTokenProvider {
         return jwt;
     }
 
-    public String validate(String jwt){
-        Claims claims = null;
+    public boolean validate(String token){
 
         try {
-            claims = Jwts.parser()
-                    .parseClaimsJws(jwt).getBody();
+            Jws<Claims> claimsJws = Jwts.parser().parseClaimsJws(token);
+            return !claimsJws.getBody().getExpiration().before(new Date());
         }catch (Exception e){
             e.printStackTrace();
-            return null;
+            return false;
         }
-
-        return claims.getSubject();
     }
 
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader("loginUser");
+    }
+
+    public Authentication getAutnetication(String token) {
+//        Claims claims = parseClaims(token);
+//        UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+        return  null;
+    }
+
+    private Claims parseClaims(String token) {
+        try{
+            return Jwts.parser().parseClaimsJws(token).getBody();
+        }catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
+    }
 }
