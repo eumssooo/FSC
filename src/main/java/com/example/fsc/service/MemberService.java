289416f965b617aa34c37c9a0755da13366a632a
KimDaHee <hee0zzang@gmail.com>
2023-09-21 10:49:34 +0900

    :art: 비밀번호 암호화, 토큰 해석 수정,

--- src/main/java/com/example/fsc/service/MemberService.java
+++ src/main/java/com/example/fsc/service/MemberService.java
@@ -1,16 +1,15 @@
 package com.example.fsc.service;
 
-import com.example.fsc.dto.memberDTO.TokenDTO;
 import com.example.fsc.entity.memberEntity.MemberEntity;
 import com.example.fsc.repository.MemberRepository;
+import com.example.fsc.security.JwtTokenProvider;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +21,9 @@ import java.util.Map;
 @Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final TokenDTO tokenDTO;
-    //private final PasswordEncoder passwordEncoder;
+    //private final TokenDTO tokenDTO;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
 
     //회원가입
@@ -32,12 +32,12 @@ public class MemberService {
         //중복 이메일 확인
         if(isEmailUnique(memberEntity.getEmail())){
             //비밀번호 암호화(sha-256)
-            //String encryptedPassword = passwordEncoder.encode(memberEntity.getPassword());
+            String encryptedPassword = passwordEncoder.encode(memberEntity.getPassword());
 
             //회원 정보 저장
             MemberEntity registerMemberEntity = new MemberEntity();
             registerMemberEntity.setEmail(memberEntity.getEmail());
-            registerMemberEntity.setPassword(memberEntity.getPassword());
+            registerMemberEntity.setPassword(encryptedPassword);
 
 
             memberRepository.save(registerMemberEntity);
@@ -55,61 +55,72 @@ public class MemberService {
         return memberEntity==null;
     }
 
-    public String generateToken(MemberEntity memberEntity){
-        System.out.println(memberEntity.getEmailId());
-        String jwt = Jwts.builder()
-                .setSubject("loginUser")
-                .claim("emailId",memberEntity.getEmailId())
-                .claim("email",memberEntity.getEmail())
-                .compact();
-        return jwt;
-    }
+//    public String generateToken(MemberEntity memberEntity){
+//        System.out.println(memberEntity.getEmailId());
+//        String jwt = Jwts.builder()
+//                .setSubject("loginUser")
+//                .claim("emailId",memberEntity.getEmailId())
+//                .claim("email",memberEntity.getEmail())
+//                .compact();
+//        return jwt;
+//    }
 
     public ResponseEntity<Map<String, String>> login(MemberEntity memberEntity, HttpServletResponse httpServletResponse) {
         //아이디 비밀번호 확인하기
         if(findByEmailAndPassword(memberEntity.getEmail(),memberEntity.getPassword())
-        ){
-            MemberEntity loginMemberEntity = setLoginMemberEntity(memberEntity.getEmail(),memberEntity.getPassword());
-            //맞을때
-            String jwt = generateToken(loginMemberEntity);
-            tokenDTO.setMemberToken(jwt);
+        ){  //로그인 성공했을 때 memberEntity 다시 정의
+            MemberEntity loginMemberEntity = setLoginMemberEntity(memberEntity.getEmail());
+            // 토큰 생성
+            String jwt = jwtTokenProvider.create(loginMemberEntity.getEmailId(),loginMemberEntity.getEmail());
+            //tokenDTO.setToken(jwt);
             httpServletResponse.addHeader("loginUser",jwt);
             Map<String,String > map = new HashMap<>();
             map.put("message","로그인이 성공적으로 완료되었습니다.");
-            //showToken(tokenDTO);
+            showToken(jwt);
             return ResponseEntity.status(200).body(map);
         }else {
             //아닐때
             System.out.println("로그인실패");
         }
+        System.out.println(4444);
         return null;
     }
 
     public boolean findByEmailAndPassword (String email, String password){
-        MemberEntity memberEntity = memberRepository.findByEmailAndPassword(email,password);
-        return memberEntity!= null;
+        MemberEntity memberEntity = memberRepository.findByEmail(email);
+        if(memberEntity!=null){
+            return passwordEncoder.matches(password,memberEntity.getPassword());
+        }
+        return false;
     }
 
-    public MemberEntity setLoginMemberEntity (String email, String password){
-        MemberEntity memberEntity = memberRepository.findByEmailAndPassword(email,password);
+    public MemberEntity setLoginMemberEntity (String email){
+        MemberEntity memberEntity = memberRepository.findByEmail(email);
         return memberEntity;
     }
 
-    public String showToken(String header){
-        System.out.println(Jwts.parser().parseClaimsJws(header).getBody().getSubject());
-        return null;
-//        String token = tokenDTO.getMemberToken();
-//        System.out.println(token);
-//        Claims claims = Jwts.parser()
-//                .parseClaimsJwt(token)
-//                .getBody();
-//        System.out.println((claims.get("emailId")));
+    public Map<String, String> showToken(String token){
 
-//        claims.get("emailId");
-        //Long emailId = Long.valueOf(claims.get("emailId"));
-//        Jwts.parser().parseClaimsJws(token).getBody().getSubject();
-//        String email = (String) claims.get("email");
-//        System.out.printf("email_id %d, email %s 님.",emailId,email);
+        Claims claims = Jwts.parser()
+               .parseClaimsJwt(token)
+               .getBody();
+        Long emailId = claims.get("emailId", Long.class); // 올바른 방법
+        String email = (String) claims.get("email");
+        System.out.println("emailId : " +emailId);
+        System.out.println("email : "+email);
+        Map<String ,String> loginInfo = new HashMap<>();
+        loginInfo.put("emailId",email);
+        loginInfo.put("email",email);
+
+        return loginInfo;
     }
 
+    public ResponseEntity<Map<String, String>> logout(MemberEntity memberEntity, HttpServletResponse httpServletResponse) {
+        httpServletResponse.setHeader(null,null);
+        Map<String,String > map = new HashMap<>();
+        map.put("message","로그아웃 되었습니다.");
+        return ResponseEntity.status(200).body(map);
+
+
+    }
 }
