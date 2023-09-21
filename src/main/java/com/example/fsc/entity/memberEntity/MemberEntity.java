cae39850d61f9ff684511923b36ea33d23ae255d
KimDaHee <hee0zzang@gmail.com>
2023-09-21 10:47:31 +0900

    :art: 멤버 엔티티 수정

--- src/main/java/com/example/fsc/entity/memberEntity/MemberEntity.java
+++ src/main/java/com/example/fsc/entity/memberEntity/MemberEntity.java
@@ -1,6 +1,7 @@
 package com.example.fsc.entity.memberEntity;
 
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -22,4 +23,8 @@ public class MemberEntity {
 
     @Column(nullable = false)
     private String password;
+
+    public void setPassword(String rawPassword, PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(rawPassword);
+    }
 }
